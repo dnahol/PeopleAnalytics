ebb6090d2dd3df8d22a4ef67c0adab007a8d6822
dnahol <dalia.nahol@gmail.com>
2019-06-23 21:13:49 -0700

    move @table to model

--- app/controllers/banner_persons_controller.rb
+++ app/controllers/banner_persons_controller.rb
@@ -1,6 +1,5 @@
 class BannerPersonsController < ApplicationController
   def index
-
     @banner_people = BannerPerson.all
     @houses = House.all
   end
@@ -25,17 +24,7 @@ class BannerPersonsController < ApplicationController
     p "membership:"
     p @membership
 
-    @table = ActiveRecord::Base.connection.execute("
-      SELECT COALESCE(l.date, h.date, a.date) AS cdate,
-      l.pts AS lpts, h.units AS hunits, a.units AS aunits, a.accepted AS acc
-      FROM (SELECT * FROM loyalty_points WHERE membership_id = #{@membership.id}) l
-      FULL OUTER JOIN (SELECT * FROM handouts WHERE membership_id = #{@membership.id}) h ON l.date = h.date
-      FULL OUTER JOIN (SELECT * FROM advisements WHERE membership_id = #{@membership.id}) a ON l.date = a.date
-      ORDER BY cdate ASC;
-      ")
-    # @table.each do |r|
-    #   p r
-    # end
+    @table = @membership.table
 
   end
   def update
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,23 @@
 class Membership < ApplicationRecord
+  include Accepted
   belongs_to :house
   belongs_to :banner_person
   has_many :loyalty_points
   has_many :advisements
   has_many :handouts
+  @table
+  attr_reader :table
+
+  after_initialize :set_table
+
+  def set_table
+    @table = ActiveRecord::Base.connection.execute("
+      SELECT COALESCE(l.date, h.date, a.date) AS cdate,
+      l.pts AS lpts, h.units AS hunits, a.units AS aunits, a.accepted AS acc
+      FROM (SELECT * FROM loyalty_points WHERE membership_id = #{self.id}) l
+      FULL OUTER JOIN (SELECT * FROM handouts WHERE membership_id = #{self.id}) h ON l.date = h.date
+      FULL OUTER JOIN (SELECT * FROM advisements WHERE membership_id = #{self.id}) a ON l.date = a.date
+      ORDER BY cdate ASC;
+      ")
+  end
 end
